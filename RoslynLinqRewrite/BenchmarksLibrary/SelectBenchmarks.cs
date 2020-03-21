bc97c3d13672eed2c72955fd9267a823afea3a1a
daniel@turing-technology.com <daniel@turing-technology.com>
2020-03-21 17:07:00 +0100

    Fixed name of folder, updated referencies

--- RoslynLinqRewrite/BenchmarksLibrary/SelectBenchmarks.cs
+++ RoslynLinqRewrite/BenchmarksLibrary/SelectBenchmarks.cs
@@ -40,7 +40,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
-using LinqRewrite.DataStructures;
+using LinqRewrite.Core;
 
 namespace BenchmarkLibrary
 {
--- RoslynLinqRewrite/TestsLibrary/SelectTests.cs
+++ RoslynLinqRewrite/TestsLibrary/SelectTests.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using LinqRewrite.DataStructures;
+using LinqRewrite.Core;
 using NUnit.Framework;
 
 namespace TestsLibrary
