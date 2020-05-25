4d7cc43900d31cf2920f29a6f1ad51c06822e325
daniel@turing-technology.com <daniel@turing-technology.com>
2020-05-25 21:30:20 +0200

    Fixed project info

--- LinqRewrite/Services/PrintService.cs
+++ LinqRewrite/Services/PrintService.cs
@@ -15,6 +15,8 @@ namespace LinqRewrite.Services
             PrintLine(@"https://gitlab.nesad.fit.vutbr.cz/xseryd00/roslyn-linq-rewrite
 
 Improved version of roslyn-linq-rewrite (https://github.com/antiufo/roslyn-linq-rewrite). This tool rewrites LINQ queries in C# code using plain procedural code, minimizing allocations and dynamic dispatch, inlining lambdas, optimizing simple math expressions and using known information for optimization. It has two run modes. First to compile rewritten code into .dll or .exe, second to rewrite the code and save it into a specified folder. For proper behavior install NuGet package LinqRewrite.Core into project which you are rewriting.
+    For basic queries which can be rewritten to simple expression (ArraySource.Select(x => x + 5).Last()) was speedup 50000x, for aggregations and simple changes of iteration was speedup several times and for queries over IEnumerable and with too few provided information was speedup up to 30% of System.Linq. There is no runtime overhead of compilation.
+    For rewrite is needed to install NuGet package LinqRewrite.Core. Repository of rewriting program https://gitlab.nesad.fit.vutbr.cz/xseryd00/roslyn-linq-rewrite/.
 
 Usage:
   roslyn-linq-rewrite --help
